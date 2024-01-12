03d241d9e8a1c75b0fb82f040255c3e42d85cd37
sucodetoM <turubazan@gmail.com>
2024-01-12 18:41:46 -0300

    update

--- screens/Ride.js
+++ screens/Ride.js
@@ -149,11 +149,11 @@ export default class RideScreen extends Component {
           //  bikeAssigned: doc.data().bike_assigned
           //});
 
-          //this.setState({
-          //  userName: doc.data().name,
-          //  userId: doc.data().id,
-          //  bikeAssigned: doc.data().bike_assigned
-          //});
+          this.setState({
+            userName: doc.data().name,
+            userId: doc.data().id,
+            bikeAssigned: doc.data().bike_assigned
+          });
 
         });
       });
@@ -174,7 +174,8 @@ export default class RideScreen extends Component {
           // se a bicicleta estiver disponível, o tipo de transação será "rented",
           // caso contrário, será "return"
 
-          //transactionType = doc.data().is_bike_available ? "rented" : "return";
+          
+        transactionType = doc.data().is_bike_available ? "rented" : "return";
           //transactionType = doc.data().is_bike_available ? "rented" ? "return";
           //transactionType === doc.data().is_bike_available ? "rented" : "return";
           //transactionType = doc.data().is_bike_available ? "rented" "return";
